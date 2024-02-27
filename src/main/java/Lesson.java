bc2a7e0f1c8ddf01aaec0315f5a475261d46e6e2
Reutkap <126346601+reutkap@users.noreply.github.com>
2024-02-27 18:57:47 +0200

    Update Lesson.java

--- src/main/java/Lesson.java
+++ src/main/java/Lesson.java
@@ -1,6 +1,5 @@
-package test;
 public class Lesson {
-	public static boolean inRow (Queue <Integer> que, int num) {
+	public static boolean q1_inRow (Queue <Integer> que, int num) {
 		boolean flag = false;
 		que.insert(null);
 		while (que.head() != null) {
@@ -45,20 +44,50 @@ public class Lesson {
 		Time r = new Time (h,m,s);
 		return r;
 	}
+	public static <T> Queue<T> q3_deleteDuplicates(Queue<T> q){
+		int c = 1;
+		q.insert(null);
+		while(q.head()!=null) {
+			T x = q.head();
+			q.insert(q.remove());
+			while(q.head()!=null) {
+				if(q.head()==x)
+					q.remove();
+				if(q.head()!=null)
+					q.insert(q.remove());
+			}
+			q.insert(q.remove());
+			for (int i = 0; i < c; i++) {
+				q.insert(q.remove());
+			}
+			c++;
+		}
+		q.remove();
+		return q;
+	}
 	public static void main(String[] args) {
-		Queue <Time> que = new Queue <Time> ();
-		que.insert(new Time(1,31,42));
-		que.insert(new Time(1,44,12));
-		que.insert(new Time(2,48,35));
-		que.insert(new Time(3,32,51));
-		que.insert(new Time(4,51,0));
-		que.insert(new Time(4,56,37));
-		que.insert(new Time(5,57,42));
-		que.insert(new Time(6,16,15));
-		System.out.println(que);
-		System.out.println("The time between the first and the last place is " + q2A(que));
-		System.out.println(que);
-
+		Queue <Time> que1 = new Queue <Time> ();
+		que1.insert(new Time(1,31,42));
+		que1.insert(new Time(1,44,12));
+		que1.insert(new Time(2,48,35));
+		que1.insert(new Time(3,32,51));
+		que1.insert(new Time(4,51,0));
+		que1.insert(new Time(4,56,37));
+		que1.insert(new Time(5,57,42));
+		que1.insert(new Time(6,16,15));
+		System.out.println(que1);
+		System.out.println("The time between the first and the last place is " + q2A(que1));
+		System.out.println(que1);
+		Queue <Character> que2 = new Queue <Character> ();
+		que2.insert('v');
+		que2.insert('g');
+		que2.insert('o');
+		que2.insert('o');
+		que2.insert('d');
+		que2.insert('p');
+		que2.insert('w');
+		que2.insert('g');
+		System.out.println(q3_deleteDuplicates(que2));
 	}
 
 }
